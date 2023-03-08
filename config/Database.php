2f74909b6f28a7e44e36be280ff7f85b7bb958c1
MrsWaifu <itsallgood4josh@yahoo.com>
2023-03-07 22:40:24 -0600

    Fix for Db Got messed up

--- config/Database.php
+++ config/Database.php
@@ -1,30 +1,38 @@
-<?php 
-  class Database {
+<?php
+    class Database {
+        private $host;
+        private $port;
+        private $dbname;
+        private $username;
+        private $password;
+        private $conn;
 
-    private $hostname = 'localhost';
-    private $port = '5432';
-    private $database = 'quotesdb';
-    private $username = 'postgres';
-    private $password = 'postgres';
-    private $conn;
 
-    // DB Connect
-    public function connect() {
-      $this->conn = null;
-  
-      $hostname = $dbparts['host'];
-      $username = $dbparts['user'];
-      $password = $dbparts['pass'];
-      $database = ltrim($dbparts['path'],'/');
+         public function __construct() {
+             $this->username = getenv('USERNAME');
+             $this->password = getenv('PASSWORD');
+             $this->dbname = getenv('DBNAME');
+             $this->host = getenv('HOST');
+             $this->port = getenv('PORT');
 
-      try {
-        $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
+         }
+         
+        //DB connect for Render
+         public function connect () {
+             if ($this->conn) {
+                 return $this->conn;
+             } else {
+                 $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+
+                 try {
+                     $this->conn = new PDO($dsn, $this->username, $this->password);
+                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                     return $this->conn;  
+                 } catch(PDOException $e) {
+                     echo 'Connection Error: ' . $e->getMessage();
+                 }
+             }
+         }
       }
-      catch(PDOException $e)
-      {
-        echo "Connection failed: " . $e->getMessage();
-      }
-    }
-  }
+
+?>
